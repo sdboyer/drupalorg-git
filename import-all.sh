671f1ed7436205897c96149562745ede0e601238
James Gilliland <neclimdul@gmail.com>
2010-08-06 22:46:10 -0500

    Allow different php locations

--- import-all.sh
+++ import-all.sh
@@ -6,11 +6,12 @@ PREFIX=contributions/modules
 DESTINATION=contributions
 LOG_PATH=logs
 DIFFLOG_PATH=difflog
+PHP="/usr/bin/php"
 
 # Remove empty repos. They're pointless in git, and the import barfs when we point at an empty directory.
 find . -maxdepth 1 -type d -empty -exec rm -r {} \;
 
 mkdir -p $LOG_PATH
-ls -d $REPOSITORY/$PREFIX/* | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "php5 import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $LOG_PATH/%.log"
+ls -d $REPOSITORY/$PREFIX/* | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "$PHP import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $LOG_PATH/%.log"
 mkdir -p $DIFFLOG_PATH
-ls -d $DESTINATION/* | sed 's/.git$//' | xargs -I% basename % | xargs --max-proc $CONCURRENCY -I% sh -c "php5 test-project.php $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $DIFFLOG_PATH/%.log"
+ls -d $DESTINATION/* | sed 's/.git$//' | xargs -I% basename % | xargs --max-proc $CONCURRENCY -I% sh -c "$PHP test-project.php $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $DIFFLOG_PATH/%.log"
