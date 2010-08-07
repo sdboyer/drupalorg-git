cedef13917f99c5f3e4c053facce22e4148c6ab7
James Gilliland <neclimdul@gmail.com>
2010-08-06 21:53:29 -0500

    Put each log in a directory and provide options

--- import-all.sh
+++ import-all.sh
@@ -4,7 +4,10 @@ CONCURRENCY=3
 REPOSITORY=/cvs/drupal-contrib
 PREFIX=contributions/modules
 DESTINATION=contributions
+LOG_PATH=logs
+DIFFLOG_PATH=difflog
 
-ls -d $REPOSITORY/$PREFIX/* | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "php5 import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee %.log"
-mkdir -p difflog
-ls -d $DESTINATION/* | sed 's/.git$//' | xargs -I% basename % | xargs --max-proc $CONCURRENCY -I% sh -c "php5 test-project.php $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee difflog/%.log"
+mkdir -p $LOG_PATH
+ls -d $REPOSITORY/$PREFIX/* | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "php5 import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $LOG_PATH/%.log"
+mkdir -p $DIFFLOG_PATH
+ls -d $DESTINATION/* | sed 's/.git$//' | xargs -I% basename % | xargs --max-proc $CONCURRENCY -I% sh -c "php5 test-project.php $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $DIFFLOG_PATH/%.log"
