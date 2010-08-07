4556a9228a5ad87f5ead182db8e990ad2b5cfc30
James Gilliland <neclimdul@gmail.com>
2010-08-06 23:03:05 -0500

    Make sure destination exists

--- import-all.sh
+++ import-all.sh
@@ -11,6 +11,7 @@ PHP="/usr/bin/php"
 # Remove empty repos. They're pointless in git, and the import barfs when we point at an empty directory.
 find . -maxdepth 1 -type d -empty -exec rm -r {} \;
 
+mkdir $DESTINATION
 mkdir -p $LOG_PATH
 ls -d $REPOSITORY/$PREFIX/* | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "$PHP import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/%.git | tee $LOG_PATH/%.log"
 mkdir -p $DIFFLOG_PATH
