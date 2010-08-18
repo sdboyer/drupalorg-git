d4f989673c8ddfe603a2b0fcc52cd334b34a32c0
Sam Boyer <drupal@samboyer.org>
2010-08-18 02:55:28 -0400

    Reworking the script so it's a bit more sane. Also added some generic functions for invoking git and log-echoing.

--- import-project.php
+++ import-project.php
@@ -1,9 +1,13 @@
 #!/usr/bin/php
 <?php
 
+// Load shared functions.
+require_once './shared.php';
+
 $config_template = realpath($argv[1]);
-$base_dir = realpath($argv[2]);
-$project = $argv[3];
+$repository_root = realpath($argv[2]);
+$source_dir = $argv[3];
+$project = array_pop(explode($source_dir));
 $destination_dir = $argv[4];
 
 // Create the destination directory, if it doesn't exist.
@@ -20,27 +24,42 @@ chdir($temp_dir);
 
 // Prepare and write the option file.
 $options = array(
-  '#DIR#' => $base_dir . '/' . $project,
+  '#DIR#' => $repository_root . '/' . $source_dir,
 );
 file_put_contents('./cvs2git.options', strtr(file_get_contents($config_template), $options));
 
 // Start the import process.
-_log("Starting the import process on the '$project' project.");
+git_log("Starting the import process on the '$project' project.");
 passthru('cvs2git --options=./cvs2git.options');
 
 // Load the data into git.
-_log("Importing '$project' project data into Git.");
+git_log("Importing '$project' project data into Git.");
 putenv('GIT_DIR=' . $destination_dir);
-system('git init');
-system('cat tmp-cvs2git/git-blob.dat tmp-cvs2git/git-dump.dat | git fast-import');
+git_invoke('git init', FALSE, $destination_dir);
+git_invoke('cat tmp-cvs2git/git-blob.dat tmp-cvs2git/git-dump.dat | git fast-import', FALSE, $destination_dir);
+
+// Branch/tag renaming functions ------------------------
+
+/**
+ * Convert all of a contrib project's branches to the new naming convention.
+ */
+function convert_contrib_project_branches($project, $destination_dir) {
+  $branches = array();
+  exec("ls " . escapeshellarg("$destination_dir/refs/heads/") . " | egrep '^(DRUPAL|master)'", $branches);
+  foreach ($branches as $branch) {
+    // @todo do the string transform, then rename the branch
+  }
+}
+
+function convert_contrib_project_tags($project, $destination_dir) {
+
+}
+
+
 
 // ------- Utility functions -----------------------------------------------
 
-function _log($message, $variables = array()) {
-  echo date('[H:i:s]') . ' ' . strtr($message, $variables) . "\n";
-}
-
 function _clean_up_import($dir) {
-  _log("Cleaning up import temp directory %dir.", array('%dir' => $dir));
+  git_log("Cleaning up import temp directory %dir.", array('%dir' => $dir));
   passthru('rm -Rf ' . escapeshellarg($dir));
 }
---
+++ shared.php
@@ -0,0 +1,39 @@
+<?php
+
+function git_invoke($command, $fail_safe = FALSE, $repository_path = NULL, $env = NULL) {
+  if (!isset($env)) {
+    $env = $_ENV;
+  }
+  if ($repository_path) {
+    $env['GIT_DIR'] = $repository_path;
+  }
+
+  $descriptor_spec = array(
+    1 => array('pipe', 'w'),
+    2 => array('pipe', 'w'),
+  );
+
+  if (DEBUG) {
+    git_log('Invoking ' . $command, 'DEB');
+  }
+
+  $process = proc_open($command, $descriptor_spec, $pipes, NULL, $env);
+  if (is_resource($process)) {
+    $stdout = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+    $stderr = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+
+    $return_code = proc_close($process);
+
+    if ($return_code != 0 && !$fail_safe) {
+      throw new Exception("Invocation of '" . $command . "' failed with return code " . $return_code .": \n" . $stdout . $stderr);
+    }
+
+    return $stdout;
+  }
+}
+
+function git_log($message, $type = 'INFO') {
+  echo '[' . date('Y-m-d H:i:s') . '] [' . $type . '] ' . $message . "\n";
+}
