c44ca861ae023224e212e999b1b73b3c0e9106e2
Sam Boyer <drupal@samboyer.org>
2010-08-18 01:21:20 -0400

    Various touchups, including the temp dir output location, variable naming used, comments, etc.

--- import-project.php
+++ import-project.php
@@ -3,32 +3,33 @@
 
 $config_template = realpath($argv[1]);
 $base_dir = realpath($argv[2]);
-$module = $argv[3];
+$project = $argv[3];
 $destination_dir = $argv[4];
 
-// Create the destination directory, if it doesn't exists.
+// Create the destination directory, if it doesn't exist.
 @mkdir($destination_dir);
 $destination_dir = realpath($destination_dir);
 
 // Create a temporary directory, and register a clean up.
-$temp_dir = realpath(trim(`mktemp -d cvs2git-import.XXXXXXXXXX`));
-register_shutdown_function('_clean_up', $temp_dir);
+$cmd = 'mktemp -dt cvs2git-import-' . escapeshellarg($project) . '.XXXXXXXXXX';
+$temp_dir = realpath(trim(`$cmd`));
+register_shutdown_function('_clean_up_import', $temp_dir);
 
 // Move to the temporary directory.
 chdir($temp_dir);
 
 // Prepare and write the option file.
 $options = array(
-  '#DIR#' => $base_dir . '/' . $module,
+  '#DIR#' => $base_dir . '/' . $project,
 );
 file_put_contents('./cvs2git.options', strtr(file_get_contents($config_template), $options));
 
 // Start the import process.
-_log("Starting the import process.");
+_log("Starting the import process on the '$project' project.");
 passthru('cvs2git --options=./cvs2git.options');
 
 // Load the data into git.
-_log("Importing data into Git.");
+_log("Importing '$project' project data into Git.");
 putenv('GIT_DIR=' . $destination_dir);
 system('git init');
 system('cat tmp-cvs2git/git-blob.dat tmp-cvs2git/git-dump.dat | git fast-import');
@@ -39,7 +40,7 @@ function _log($message, $variables = array()) {
   echo date('[H:i:s]') . ' ' . strtr($message, $variables) . "\n";
 }
 
-function _clean_up($dir) {
-  _log("Cleaning up directory %dir.", array('%dir' => $dir));
+function _clean_up_import($dir) {
+  _log("Cleaning up import temp directory %dir.", array('%dir' => $dir));
   passthru('rm -Rf ' . escapeshellarg($dir));
 }
