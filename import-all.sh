b340d3a1b4d618dde1a77cd9629f27b596591646
Sam Boyer <drupal@samboyer.org>
2010-09-08 10:55:52 -0700

    Change top-level namespace from 'projects' to 'project', to coincide with the naming on d.o

--- import-all.sh
+++ import-all.sh
@@ -5,17 +5,17 @@ REPOSITORY=/var/git/cvsmirror # replace with path to the root of the local repos
 DESTINATION=/var/git/repositories
 PHP="/usr/bin/php"
 
-mkdir -p $DESTINATION/projects
+mkdir -p $DESTINATION/project
 
 # do special-case handling for docs, tricks, and finally core. Do these first in the background because they take a while.
-$PHP import-project.php ./cvs2git.options $REPOSITORY contributions/docs $DESTINATION/projects/docs.git &
-$PHP import-project.php ./cvs2git.options $REPOSITORY contributions/tricks $DESTINATION/projects/tricks.git &
-$PHP import-project.php ./cvs2git.options $REPOSITORY drupal $DESTINATION/projects/drupal.git &
+$PHP import-project.php ./cvs2git.options $REPOSITORY contributions/docs $DESTINATION/project/docs.git &
+$PHP import-project.php ./cvs2git.options $REPOSITORY contributions/tricks $DESTINATION/project/tricks.git &
+$PHP import-project.php ./cvs2git.options $REPOSITORY drupal $DESTINATION/project/drupal.git &
 
-# migrate all the parent dirs for which each child receives a repo in the shared, top-level namespace (projects)
+# migrate all the parent dirs for which each child receives a repo in the shared, top-level namespace (project)
 for TYPE in modules themes theme-engines profiles; do
     PREFIX="contributions/$TYPE"
-    find $REPOSITORY/$PREFIX/ -mindepth 1 -maxdepth 1 -type d -not -empty | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "$PHP import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/projects/%.git"
+    find $REPOSITORY/$PREFIX/ -mindepth 1 -maxdepth 1 -type d -not -empty | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "$PHP import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/project/%.git"
 done
 
 # migrate sandboxes into their frozen location
