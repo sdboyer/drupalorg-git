849cd51b3f4c5363155d90cf93ea4b214f7c481a
Sam Boyer <drupal@samboyer.org>
2010-12-29 16:45:55 -0800

    Update the import-all.sh wrapper to be sensitive to environment variables, and namespace the variable names for safety.

--- import-all.sh
+++ import-all.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-CONCURRENCY=8 # set to the number of cores you want to pwn with the migration process
-REPOSITORY=/var/git/cvsmirror # replace with path to the root of the local repository
-DESTINATION=/var/git/repositories
-PHP="/usr/bin/php"
+if [ ! $C2G_CONCURRENCY ]; then
+    CONCURRENCY=8 # set to the number of cores you want to pwn with the migration process
+fi
+if [ ! $C2G_REPOSITORY ]; then
+    REPOSITORY=/var/git/cvsmirror # replace with path to the root of the local repository
+fi
+if [ ! $C2G_DESTINATION ]; then
+    DESTINATION=/var/git/repositories
+fi
+if [ ! $C2G_PHP ]; then
+    PHP="/usr/bin/php"
+fi
 
-mkdir -p $DESTINATION/project
+mkdir -p $C2G_DESTINATION/project
 
 # do special-case handling for docs, tricks, and finally core. Do these first in the background because they take a while.
-$PHP import-project.php ./cvs2git.options $REPOSITORY contributions/docs $DESTINATION/project/docs.git &
-$PHP import-project.php ./cvs2git.options $REPOSITORY contributions/tricks $DESTINATION/project/tricks.git &
-$PHP import-project.php ./cvs2git.options $REPOSITORY drupal $DESTINATION/project/drupal.git &
+$C2G_PHP import-project.php ./cvs2git.options $C2G_REPOSITORY contributions/docs $C2G_DESTINATION/project/docs.git &
+$C2G_PHP import-project.php ./cvs2git.options $C2G_REPOSITORY contributions/tricks $C2G_DESTINATION/project/tricks.git &
+$C2G_PHP import-project.php ./cvs2git.options $C2G_REPOSITORY drupal $C2G_DESTINATION/project/drupal.git &
 
 # migrate all the parent dirs for which each child receives a repo in the shared, top-level namespace (project)
 for TYPE in modules themes theme-engines profiles; do
     PREFIX="contributions/$TYPE"
-    find $REPOSITORY/$PREFIX/ -mindepth 1 -maxdepth 1 -type d -not -empty | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $CONCURRENCY -I% sh -c "$PHP import-project.php ./cvs2git.options $REPOSITORY $PREFIX/% $DESTINATION/project/%.git"
+    find $C2G_REPOSITORY/$PREFIX/ -mindepth 1 -maxdepth 1 -type d -not -empty | xargs -I% basename % | egrep -v "Attic" | xargs --max-proc $C2G_CONCURRENCY -I% sh -c "$C2G_PHP import-project.php ./cvs2git.options $C2G_REPOSITORY $PREFIX/% $C2G_DESTINATION/project/%.git"
 done
 
