2a321840dcc3bc224cb6a561494d19c3cc6346e8
Sam Boyer <drupal@samboyer.org>
2011-01-16 22:42:04 -0500

    Split the calls into a huge number of smaller drush sync calls, helping us with bloat and timeout problems during the massive repo parsing.

--- run-drush-sync.php
+++ run-drush-sync.php
@@ -6,11 +6,16 @@ $proc_count = empty($argv[2]) ? 8 : (int) $argv[2];
 $raw_dirs = glob('/var/git/stagingrepos/project/*.git');
 $dirs = preg_replace('#^/var/git/stagingrepos/project/(.*)\.git$#', '\1', $raw_dirs);
 
+// Pull out the biggest ones to run in just one process, keep things more even
+$biggies = array('drupal', 'ecommerce', 'views');
+$dirs = array_diff($dirs, $biggies);
+
 $dirs_num = count($dirs);
 
-$chunks = array_chunk($dirs, $dirs_num / $proc_count);
+$chunks = array_chunk($dirs, $dirs_num / ($proc_count - 1));
+array_unshift($chunks, $biggies);
 
-// Run eight forked subprocesses
+// Run forked subprocesses
 $success = TRUE;
 $forks = 0;
 for ($i = 0; $i < $proc_count; ++$i) {
@@ -25,9 +30,13 @@ for ($i = 0; $i < $proc_count; ++$i) {
   }
   else {
     // Child
-    $repos = implode(',', $chunks[$i]);
-    // give feedback on list of repos to be imported
-    passthru(escapeshellcmd($drush . ' vcapi-parse-logs ' . escapeshellarg($repos)));
+    $child_list = $chunks[$i];
+    $chunk_count = (int) (count($child_list) / 3);
+    $child_chunks = empty($chunk_count) ? $child_list : array_chunk($child_list, $chunk_count);
+    foreach ($child_chunks as $chunk) {
+      $repos = implode(',', $chunk);
+      passthru(escapeshellcmd($drush . ' vcapi-parse-logs ' . escapeshellarg($repos)));
+    }
     exit;
   }
 }
