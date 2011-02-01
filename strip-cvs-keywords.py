2fb8bff5722ce6daf6f10df01428718c61e4466d
Sam Boyer <drupal@samboyer.org>
2011-02-01 21:11:20 +0000

    Fix sed call for non-BSD sed compatibility.

--- strip-cvs-keywords.py
+++ strip-cvs-keywords.py
@@ -70,7 +70,7 @@ def main():
             name, extension = os.path.splitext(filename)
             if extension.lower() in EXTENSIONS or name in FILE_NAMES:
                 abs_path = os.path.realpath(os.path.join(path, root, filename))
-                subprocess.Popen(('sed', '-Ei', '-f', SED_FILE, abs_path))
+                subprocess.Popen(('sed', '-i', '-f', SED_FILE, abs_path))
 
 if __name__ == "__main__":
     main()
