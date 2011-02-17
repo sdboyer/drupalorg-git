6d5da32b3b0b244a06e507730f0699f498a296a6
Sam Boyer <drupal@samboyer.org>
2011-02-16 21:55:11 -0800

    No translation stripping for Drupal projects OR translations. Covers the bases nicely.

--- dump-project-info.drush.php
+++ dump-project-info.drush.php
@@ -1,6 +1,6 @@
 <?php
 
-$result = db_query('SELECT cp.directory, p.uri, COALESCE((tn.tid != 29), 1) as strip_trans, p.nid FROM project_projects AS p INNER JOIN cvs_projects AS cp ON p.nid = cp.nid INNER JOIN node AS n on p.nid = n.nid LEFT JOIN term_node tn ON n.vid = tn.vid AND tn.tid IN (13, 14, 15, 29, 32, 96)');
+$result = db_query('SELECT cp.directory, p.uri, COALESCE((tn.tid NOT IN (13, 29)), 1) as strip_trans, p.nid FROM project_projects AS p INNER JOIN cvs_projects AS cp ON p.nid = cp.nid INNER JOIN node AS n on p.nid = n.nid LEFT JOIN term_node tn ON n.vid = tn.vid AND tn.tid IN (13, 14, 15, 29, 32, 96)');
 
 $fileobj = new SplFileObject(dirname(__FILE__) . '/project-migrate-info', 'w');
 while ($row = db_fetch_object($result)) {
